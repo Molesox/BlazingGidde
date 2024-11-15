b6674f0ae3c859c57ee5745c36cc8ff413d63852
Daniel <daniel.sanz@alpsoft.ch>
2024-11-15 17:55:03 +0100

    syncfusion grids - start

--- BlazingGidde.Client/Program.cs
+++ BlazingGidde.Client/Program.cs
@@ -4,10 +4,14 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzU3OTEyMEAzMjM3MmUzMDJlMzBYdGxweHFSTXJWVkVhMkZ2Vm11WkJCcERmZkEyYXd6YUgwYW5WTUtSUGVvPQ==");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
@@ -16,5 +20,7 @@ builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStatePr
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddScoped<AspNetUserManager>();
+
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
