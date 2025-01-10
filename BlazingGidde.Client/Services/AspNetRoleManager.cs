6421ea7bc3eacb0fb607baa26efec60a79dad336
David Tortosa <pintajarto@gmail.com>
2025-01-10 12:21:38 +0100

    añadidos DTOs de flowRoles y modificado ASPNetRoles para que funcione con dto

--- BlazingGidde.Client/Services/AspNetRoleManager.cs
+++ BlazingGidde.Client/Services/AspNetRoleManager.cs
@@ -1,3 +1,4 @@
+using BlazingGidde.Shared.DTOs.FlowCheck;
 using BlazingGidde.Shared.Models.FlowCheck;
 using BlazingGidde.Shared.Models.Identity;
 using DevExpress.Blazor;
@@ -5,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace BlazingGidde.Client.Services;
 
-public class AspNetRoleManager : APIRepository<FlowRole, string>
+public class AspNetRoleManager : APIRepository<FlowRoleDto, string>
 	{
 		public AspNetRoleManager(HttpClient _http, IToastNotificationService toastNotificationService)
 			: base(_http, "AppRoles", toastNotificationService)
--- BlazingGidde.Server/Controllers/Identity/AppRolesController.cs
+++ BlazingGidde.Server/Controllers/Identity/AppRolesController.cs
@@ -6,23 +6,22 @@ using BlazingGidde.Server.Data;
 using BlazingGidde.Shared.Models.Identity;
 using BlazingGidde.Shared.Repository;
 using BlazingGidde.Shared.Models.FlowCheck;
+using BlazingGidde.Shared.DTOs.FlowCheck;
+using BlazingGidde.Shared.DTOs.FlowCheck.Request;
 
 namespace BlazingGidde.Server.Controllers.Identity
 {
 
-	[ApiController]
-	[Route("[controller]")]
-	[Authorize]
-	public class AppRolesController : OnaBaseController<FlowRole, string, ApplicationDbContext>
-	{
-		public AppRolesController(IRepository<FlowRole> repository,
-			ILogger<OnaBaseController<FlowRole, string, ApplicationDbContext>> logger
-		)
-			: base(repository, logger)
-		{
-		}
-
-
-	}
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class AppRolesController : OnaBaseController<FlowRole, string, ApplicationDbContext, FlowRoleDto, CreateFlowRoleDto>
+    {
+        public AppRolesController(IRepository<FlowRole> repository, 
+            ILogger<OnaBaseController<FlowRole, string, ApplicationDbContext, FlowRoleDto, CreateFlowRoleDto, CreateFlowRoleDto, FlowRoleDto, FlowRoleDto>> logger) 
+            : base(repository, logger)
+        {
+        }
+    }
 }
 
