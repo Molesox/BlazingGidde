a9595d12e0574e5059461fc63a0e16c74257766a
David Tortosa <pintajarto@gmail.com>
2025-01-25 13:20:05 +0100

    inicio pagina perfil, y añadido nombre al logindisplay

--- BlazingGidde.Client/Services/AspNetUserManager.cs
+++ BlazingGidde.Client/Services/AspNetUserManager.cs
@@ -1,19 +1,19 @@
-﻿
-
-using BlazingGidde.Shared.DTOs.FlowCheck;
+﻿using BlazingGidde.Shared.DTOs.FlowCheck;
 using BlazingGidde.Shared.DTOs.FlowCheck.Request;
 using BlazingGidde.Shared.Models.FlowCheck;
 using DevExpress.Blazor;
+using System.Net.Http.Json;
+
+using static System.Net.WebRequestMethods;
 
 namespace BlazingGidde.Client.Services
 {
     public class AspNetUserManager : APIRepository<FlowUser, string, FlowUserDto, CreateFlowUserDto>
-	{
-		public AspNetUserManager(HttpClient _http, IToastNotificationService toastNotificationService)
-			: base(_http, "AppUsers", toastNotificationService)
-		{ }
-	}
+    {
+        // Constructor que pasa las dependencias a la clase base
+        public AspNetUserManager(HttpClient http, IToastNotificationService toastNotificationService)
+            : base(http, "AppUsers", toastNotificationService)
+        { }
 
-
-	
+    }
 }
